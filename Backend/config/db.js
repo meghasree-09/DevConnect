d9cdea048096916d2bb5c9f2e12e54a7d3156995
meghasree-09 <saimeghanasree09@gmail.com>
2026-07-18 00:03:33 +0530

    Setup MongoDB configuration

---
+++ Backend/config/db.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import dns from "node:dns";
+import dotenv from "dotenv";
+dotenv.config();
+
+
+dns.setDefaultResultOrder("ipv4first");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB Connected Successfully");
+  } catch (error) {
+    console.error("❌ Database connection failed");
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
