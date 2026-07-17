40df0649d8eecf57406e1ff54d57c1c317607da2
meghasree-09 <saimeghanasree09@gmail.com>
2026-07-17 23:53:48 +0530

    Implement CRUD operations in user controller

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,117 +1,109 @@
-import users from "../data/users.js";
+import User from "../models/users.js";
 
-// GET all users
-export const getUsers = (
-  req,
-  res
-) => {
-  res.json(users);
+export async function getUser(req,res){
+    try{
+        const users=await User.find();
+        res.status(200).json({
+            success:true,
+            users
+        })
+    }catch(error){
+        res.status(500).json({
+            success:false,
+            message:error.message,
+        })
+    }
+
 };
 
-// GET user by ID
-export const getUserById = (
-  req,
-  res
-) => {
-  const id = Number(
-    req.params.id
-  );
+export async function getUserById(req,res){
+   try{
+        const user=await User.findById(req.params.id);
+         if(!user){
+        return res.status(404).json({
+            success:false,
+            message:"user not found"
+        });
+    };
+    res.status(200).json({
+        success:true,
+        user
+    })
+        
+    }catch(error){
+        res.status(500).json({
+            success:false,
+            message:error.message,
+            
 
-  const user =
-    users.find(
-      (u) => u.id === id
-    );
-
-  if (!user) {
-    return res.status(404).json({
-      message:
-        "User not found"
-    });
-  }
-
-  res.json(user);
-};
-
-// POST create user
-export const createUser = (
-  req,
-  res
-) => {
-
-  const newUser = {
-    id: users.length + 1,
-    name: req.body.name,
-    email: req.body.email
+        })
+    }
   };
-
-  users.push(newUser);
-
-  res.status(201).json({
-    message:
-      "User Created Successfully",
-    newUser
-  });
-};
-
-export const updateUser = (
-  req,
-  res
-) => {
-
-  const id = Number(
-    req.params.id
-  );
-
-  const user =
-    users.find(
-      (u) => u.id === id
-    );
-
-  if (!user) {
-    return res.status(404).json({
-      message: "User not found"
+export async function addUser(req,res){
+  try{
+        const user=await User.create(req.body);
+        res.status(201).json({
+            success:true,
+            message:"user registered successfully",
+            user
+        })
+    }
+    catch(error){
+    res.status(500).json({
+        success:false,
+        message:error.message,
+    
     });
-  }
-
-  user.name =
-    req.body.name ||
-    user.name;
-
-  user.email =
-    req.body.email ||
-    user.email;
-
-  res.json({
-    message:
-      "User Updated Successfully",
-    user
-  });
+}
 };
-export const deleteUser = (
-  req,
-  res
-) => {
+export async function updateUser(req,res){
+  try{
+        const user =await User.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {
+            new:true,
+            runValidators:true
+        }
 
-  const id = Number(
-    req.params.id
-  );
+        );
+        
+    if(!user){
+        return res.status(400).json({
+            success:false,
+            message:"user not found"
+        })
+    }
+    res.status(200).json({
+        success:true,
+        message:"user updated successfully",
+        user
 
-  const index =
-    users.findIndex(
-      (u) => u.id === id
-    );
-
-  if (index === -1) {
-    return res.status(404).json({
-      message:
-        "User not found"
     });
-  }
-
-  users.splice(index, 1);
-
-  res.json({
-    message:
-      "User Deleted Successfully"
-  });
+}catch(error){
+    res.status(500).json({
+        success:false,
+        message:error.message,
+    });
+}
 };
+export async function deleteUser(req,res){
+    try{
+        const user =await User.findByIdAndDelete(req.params.id);
+        if(!user){
+            return res.status(404).json({
+                success:false,
+                message:"user not found"
+            });
+        }
+        res.status(200).json({
+            success:true,
+            message:"user deleted successfully"
+        })
+    }catch(error){
+        res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+  };
