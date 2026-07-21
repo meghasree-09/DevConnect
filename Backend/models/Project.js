ec8af62895688bb5c8f97dab96ccdc757a20327e
meghasree-09 <saimeghanasree09@gmail.com>
2026-07-21 23:56:46 +0530

    add projectmodel

---
+++ Backend/models/Project.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const projectSchema =
+  new mongoose.Schema(
+    {
+      title: {
+        type: String,
+        required: true,
+      },
+
+      description: {
+        type: String,
+        required: true,
+      },
+
+      technologies: [
+        {
+          type: String,
+        },
+      ],
+
+      image: {
+        type: String,
+      },
+
+      githubLink: {
+        type: String,
+      },
+
+      liveLink: {
+        type: String,
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
+
+const Project =
+  mongoose.model(
+    "Project",
+    projectSchema
+  );
+
+export default Project;
