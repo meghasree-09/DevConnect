f600de59027c608f9dd5780ddf6c393ed725c628
meghasree-09 <saimeghanasree09@gmail.com>
2026-07-21 23:48:29 +0530

    updated usercontroller features

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,109 +1,105 @@
 import User from "../models/users.js";
 
-export async function getUser(req,res){
-    try{
-        const users=await User.find();
-        res.status(200).json({
-            success:true,
-            users
-        })
-    }catch(error){
-        res.status(500).json({
-            success:false,
-            message:error.message,
-        })
+export const addUser =
+  async (req, res) => {
+    try {
+      const user =
+        await User.create(
+          req.body
+        );
+
+      res.status(201).json(user);
     }
-
-};
-
-export async function getUserById(req,res){
-   try{
-        const user=await User.findById(req.params.id);
-         if(!user){
-        return res.status(404).json({
-            success:false,
-            message:"user not found"
-        });
-    };
-    res.status(200).json({
-        success:true,
-        user
-    })
-        
-    }catch(error){
-        res.status(500).json({
-            success:false,
-            message:error.message,
-            
-
-        })
+    catch (error) {
+      res.status(500).json({
+        message:
+          error.message,
+      });
     }
   };
-export async function addUser(req,res){
-  try{
-        const user=await User.create(req.body);
-        res.status(201).json({
-            success:true,
-            message:"user registered successfully",
-            user
-        })
-    }
-    catch(error){
-    res.status(500).json({
-        success:false,
-        message:error.message,
-    
-    });
-}
-};
-export async function updateUser(req,res){
-  try{
-        const user =await User.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        {
-            new:true,
-            runValidators:true
-        }
 
-        );
-        
-    if(!user){
-        return res.status(400).json({
-            success:false,
-            message:"user not found"
-        })
-    }
-    res.status(200).json({
-        success:true,
-        message:"user updated successfully",
+export const loginUser =
+  async (req, res) => {
+    try {
+      const {
+        email,
+        password,
+      } = req.body;
+
+      const user =
+        await User.findOne({
+          email,
+        });
+
+      if (!user) {
+        return res
+          .status(404)
+          .json({
+            message:
+              "User not found",
+          });
+      }
+
+      if (
+        user.password !==
+        password
+      ) {
+        return res
+          .status(400)
+          .json({
+            message:
+              "Invalid Password",
+          });
+      }
+
+      res.status(200).json(
         user
+      );
+    }
+    catch (error) {
+      res.status(500).json({
+        message:
+          error.message,
+      });
+    }
+  };
 
-    });
-}catch(error){
-    res.status(500).json({
-        success:false,
-        message:error.message,
-    });
-}
-};
-export async function deleteUser(req,res){
-    try{
-        const user =await User.findByIdAndDelete(req.params.id);
-        if(!user){
-            return res.status(404).json({
-                success:false,
-                message:"user not found"
-            });
-        }
-        res.status(200).json({
-            success:true,
-            message:"user deleted successfully"
-        })
-    }catch(error){
-        res.status(500).json({
-            success:false,
-            message:error.message
-        })
+export const forgotPassword =
+  async (req, res) => {
+    try {
+      const {
+        email,
+        password,
+      } = req.body;
+
+      const user =
+        await User.findOne({
+          email,
+        });
+
+      if (!user) {
+        return res
+          .status(404)
+          .json({
+            message:
+              "Email not found",
+          });
+      }
+
+      user.password =
+        password;
+
+      await user.save();
+
+      res.status(200).json({
+        message:
+          "Password Updated Successfully",
+      });
+    }
+    catch (error) {
+      res.status(500).json({
+        message:
+          error.message,
+      });
     }
   };
