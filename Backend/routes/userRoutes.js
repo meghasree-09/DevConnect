82417527aa3516125ebea7f577334a5c87dad66c
meghasree-09 <saimeghanasree09@gmail.com>
2026-07-22 23:56:49 +0530

    enhanced user routes

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,13 +1,38 @@
 import express from "express";
 
 import {
+  getUsers,
+  getUserById,
   addUser,
+  updateUser,
+  deleteUser,
   loginUser,
   forgotPassword,
-} from "../controllers/userController.js";
+}
+from "../controllers/userController.js";
 
 const router =
   express.Router();
+
+router.get(
+  "/",
+  getUsers
+);
+
+router.get(
+  "/:id",
+  getUserById
+);
+
+router.put(
+  "/:id",
+  updateUser
+);
+
+router.delete(
+  "/:id",
+  deleteUser
+);
 
 router.post(
   "/",
