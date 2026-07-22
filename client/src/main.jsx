2a28eedb4c29a1101b6ed7a0c3a63d58539bef32
meghasree-09 <saimeghanasree09@gmail.com>
2026-07-23 00:19:59 +0530

    Update main application entry point

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
   <BrowserRouter>
-    <App />
+  <AuthProvider>
+     <App />
+
+  </AuthProvider>
+   
   </BrowserRouter>
 );
