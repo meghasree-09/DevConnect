786e4c679f1c08515ffba183447a885c33ebbcdb
meghasree-09 <saimeghanasree09@gmail.com>
2026-07-24 22:54:39 +0530

    add community model

--- Backend/models/Community.js
+++ Backend/models/Community.js
@@ -1,32 +1,62 @@
 import mongoose from "mongoose";
 
-const communitySchema =
-  new mongoose.Schema(
-    {
-      name: {
-        type: String,
-        required: true,
-      },
-
-      description: {
-        type: String,
-        required: true,
-      },
-
-      members: [
-        {
-          type:
-            mongoose.Schema.Types.ObjectId,
-          ref: "User",
-        },
-      ],
+const communitySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    {
-      timestamps: true,
-    }
-  );
 
-export default mongoose.model(
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    category: {
+      type: String,
+      default: "",
+    },
+
+    image: {
+      type: String,
+      default: "",
+    },
+
+    rules: [
+      {
+        type: String,
+      },
+    ],
+
+    technologies: [
+      {
+        type: String,
+      },
+    ],
+
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
+
+    members: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Community = mongoose.model(
   "Community",
   communitySchema
 );
+
+export default Community;
