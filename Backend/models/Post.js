0586c1ded45c77d7c377f0595d7061f354f94892
meghasree-09 <saimeghanasree09@gmail.com>
2026-07-24 23:56:31 +0530

    add post model

---
+++ Backend/models/Post.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    text: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const postSchema = new mongoose.Schema(
+  {
+    community: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Community",
+      required: true,
+    },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 1000,
+    },
+
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    comments: [commentSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
