0ce2a5ebcff203df97a6538ddafc8ac5a5307b01
meghasree-09 <saimeghanasree09@gmail.com>
2026-07-25 00:30:54 +0530

    add community api

--- client/src/api/communityapi.js
+++ client/src/api/communityapi.js
@@ -1,35 +1,52 @@
 import api from "./api";
 
-export const getCommunities =
-  async () => {
+export const getCommunities = async () => {
 
-    const response =
-      await api.get(
-        "/communities"
-      );
+  const response = await api.get("/communities");
 
-    return response.data;
+  return response.data;
+
 };
 
-export const createCommunity =
-  async (community) => {
+export const createCommunity = async (community) => {
 
-    const response =
-      await api.post(
-        "/communities",
-        community
-      );
+  const response = await api.post(
+    "/communities",
+    community
+  );
 
-    return response.data;
+  return response.data;
+
 };
-export const joinCommunity =
-  async (data) => {
 
-    const response =
-      await api.post(
-        "/communities/join",
-        data
-      );
+export const joinCommunity = async (data) => {
 
-    return response.data;
+  const response = await api.post(
+    "/communities/join",
+    data
+  );
+
+  return response.data;
+
+};
+
+export const getCommunity = async (id) => {
+
+  const response = await api.get(
+    `/communities/${id}`
+  );
+
+  return response.data;
+
+};
+
+export const updateCommunity = async (id, data) => {
+
+  const response = await api.put(
+    `/communities/${id}`,
+    data
+  );
+
+  return response.data;
+
 };
