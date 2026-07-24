46c1c4295b58a06180cb7d1deb96077cae4d5719
meghasree-09 <saimeghanasree09@gmail.com>
2026-07-24 23:55:54 +0530

    add notification model

---
+++ Backend/models/Notification.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema(
+  {
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    type: {
+      type: String,
+      enum: [
+        "JOIN_REQUEST",
+        "REQUEST_ACCEPTED",
+        "REQUEST_REJECTED",
+        "TASK_ASSIGNED",
+        "TASK_COMPLETED",
+      ],
+      required: true,
+    },
+
+    message: {
+      type: String,
+      required: true,
+    },
+
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Notification = mongoose.model(
+  "Notification",
+  notificationSchema
+);
+
+export default Notification;
