ba709c37a0672ab5cfd66954793629dd20b1783f
meghasree-09 <saimeghanasree09@gmail.com>
2026-07-24 23:40:38 +0530

    add team route

--- Backend/routes/teamRequestRoutes.js
+++ Backend/routes/teamRequestRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-
+import { protect } from "../middleware/authMiddleware.js";
 import {
   createRequest,
   getRequests,
+  updateRequest
 }
 from "../controllers/teamRequestController.js";
 
@@ -10,13 +11,11 @@ const router =
   express.Router();
 
 router.post(
-  "/",
+  "/",protect,
   createRequest
 );
 
-router.get(
-  "/",
-  getRequests
-);
+router.get("/:userId",protect, getRequests);
+router.put("/:id",protect, updateRequest);
 
 export default router;
