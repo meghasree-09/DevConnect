4a53989a4b00ba136dff50fa2809fc535b430ad0
meghasree-09 <saimeghanasree09@gmail.com>
2026-07-25 00:29:33 +0530

    add project api

--- client/src/api/projectapi.js
+++ client/src/api/projectapi.js
@@ -1,38 +1,31 @@
 import api from "./api";
 
-export const getProjects =
-  async () => {
-
-    const response =
-      await api.get(
-        "/projects"
-      );
-
-    return response.data;
-
+// Get all projects
+export const getProjects = async () => {
+  const response = await api.get("/projects");
+  return response.data;
 };
 
-export const getProject =
-  async (id) => {
-
-    const response =
-      await api.get(
-        `/projects/${id}`
-      );
-
-    return response.data;
-
+// Get single project
+export const getProject = async (id) => {
+  const response = await api.get(`/projects/${id}`);
+  return response.data;
 };
 
-export const createProject =
-  async (project) => {
+// Create project
+export const createProject = async (project) => {
+  const response = await api.post("/projects", project);
+  return response.data;
+};
 
-    const response =
-      await api.post(
-        "/projects",
-        project
-      );
+// Update project
+export const updateProject = async (id, project) => {
+  const response = await api.put(`/projects/${id}`, project);
+  return response.data;
+};
 
-    return response.data;
-
+// Delete project
+export const deleteProject = async (id) => {
+  const response = await api.delete(`/projects/${id}`);
+  return response.data;
 };
