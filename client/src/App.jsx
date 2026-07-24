21f713d3591cf8c9580c2ae4bd70a4e1fcd59d58
meghasree-09 <saimeghanasree09@gmail.com>
2026-07-25 01:39:32 +0530

    configure App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,13 @@ import Homepage from "./Pages/Homepage/Homepage";
 import Features from "./Pages/Features/Features";
 import Projects from "./Pages/Projects/Projects";
 import Communities from "./Pages/Communities/Communities";
+import JoinedCommunities from "./Pages/Communities/JoinedCommunities";
+import CommunityDetails from "./Pages/Communities/CommunityDetails";
+import UserProfile from "./Pages/Users/UserProfile";
 import Developers from "./Pages/Developers/Developers";
 import DeveloperProfile from "./Pages/Developers/DeveloperProfile";
 import CreateDeveloper from "./Pages/Developers/CreateDeveloper";
+
 import Contact from "./Pages/Contact/Contact";
 
 import Login from "./Pages/Login/Login";
@@ -18,11 +22,17 @@ import EditUser from "./Pages/EditUser/EditUser";
 import UserDashboard from "./Pages/Dashboard/UserDashboard";
 import LeadDashboard from "./Pages/Dashboard/LeadDashboard";
 import AdminDashboard from "./Pages/Dashboard/AdminDashboard";
-
+import ProjectChat from "./Pages/Chat/ProjectChat";
 import CreateProjects from "./Pages/Project/CreateProjects";
 import ManageProjects from "./Pages/Project/ManageProjects";
 import ViewProject from "./Pages/Project/ViewProject";
 import TeamRequests from "./Pages/Project/TeamRequests";
+import ManageTasks from "./Pages/Project/ManageTasks";
+import Notifications from "./Pages/Notifications/Notifications";
+import MyTasks from "./Pages/Project/MyTasks";
+import MyTeam from "./Pages/Project/MyTeam";
+
+import ProtectedRoute from "./components/ProtecedRoute";
 
 import ErrorPage from "./Pages/ErrorPage/ErrorPage";
 
@@ -30,111 +40,104 @@ function App() {
   return (
     <Routes>
       {/* Home */}
-      <Route
-        path="/"
-        element={<Homepage />}
-      />
-
-      <Route
-        path="/features"
-        element={<Features />}
-      />
-
-      <Route
-        path="/projects"
-        element={<Projects />}
-      />
-
-      <Route
-        path="/communities"
-        element={<Communities />}
-      />
-
-      <Route
-        path="/developers"
-        element={<Developers />}
-      />
-
-      <Route
-        path="/developer/:id"
-        element={<DeveloperProfile />}
-      />
-
-      <Route
-        path="/create-developer"
-        element={<CreateDeveloper />}
-      />
-
-      <Route
-        path="/contact"
-        element={<Contact />}
-      />
+      <Route path="/" element={<Homepage />} />
+      <Route path="/features" element={<Features />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/communities" element={<Communities />} />
+      <Route path="/joined-communities" element={<JoinedCommunities />} />
+      <Route path="/community/:id" element={<CommunityDetails />} />
+      <Route path="/developers" element={<Developers />} />
+      <Route path="/developer/:id" element={<DeveloperProfile />} />
+      <Route path="/create-developer" element={<CreateDeveloper />} />
+      <Route path="/contact" element={<Contact />} />
 
       {/* Authentication */}
-      <Route
-        path="/login"
-        element={<Login />}
-      />
-
-      <Route
-        path="/register"
-        element={<Register />}
-      />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
 
       {/* Users */}
-      <Route
-        path="/users"
-        element={<Users />}
-      />
-
-      <Route
-        path="/edit/:id"
-        element={<EditUser />}
-      />
+      <Route path="/users" element={<Users />} />
+      <Route path="/edit/:id" element={<EditUser />} />
 
       {/* Dashboards */}
+
       <Route
         path="/user"
-        element={<UserDashboard />}
+        element={
+          <ProtectedRoute role="user">
+            <UserDashboard />
+          </ProtectedRoute>
+        }
       />
+
+      
 
       <Route
         path="/lead"
-        element={<LeadDashboard />}
+        element={
+          <ProtectedRoute role="projectLead">
+            <LeadDashboard />
+          </ProtectedRoute>
+        }
       />
 
       <Route
         path="/admin"
-        element={<AdminDashboard />}
+        element={
+          <ProtectedRoute role="admin">
+            <AdminDashboard />
+          </ProtectedRoute>
+        }
       />
 
       {/* Projects */}
+
       <Route
         path="/create-project"
-        element={<CreateProjects />}
+        element={
+          <ProtectedRoute role="projectLead">
+            <CreateProjects />
+          </ProtectedRoute>
+        }
       />
 
       <Route
         path="/manage-projects"
-        element={<ManageProjects />}
+        element={
+          <ProtectedRoute role="projectLead">
+            <ManageProjects />
+          </ProtectedRoute>
+        }
       />
+      <Route path="/tasks/:projectId" element={<ManageTasks />} />
+      <Route path="/chat/:projectId" element={<ProjectChat />} />
+      <Route path="/my-tasks" element={<MyTasks />} />
+      <Route path="/projects/:id" element={<ViewProject />} />
 
       <Route
-        path="/projects/:id"
-        element={<ViewProject />}
+        path="/team/:projectId"
+        element={
+          <ProtectedRoute role="projectLead">
+            <MyTeam />
+          </ProtectedRoute>
+        }
       />
-
 
       <Route
         path="/team-requests"
-        element={<TeamRequests />}
+        element={
+          <ProtectedRoute role="projectLead">
+            <TeamRequests />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/notifications"
+        element={<Notifications />}
       />
 
-      {/* Error Page */}
-      <Route
-        path="*"
-        element={<ErrorPage />}
-      />
+      {/* Error */}
+      <Route path="*" element={<ErrorPage />} />
     </Routes>
   );
 }
