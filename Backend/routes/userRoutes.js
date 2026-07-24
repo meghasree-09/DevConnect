7958dd395a6217a8ea4c12b70cc80639b506d15e
meghasree-09 <saimeghanasree09@gmail.com>
2026-07-24 23:41:22 +0530

    add user routes

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import { protect } from "../middleware/authMiddleware.js";
 import {
   getUsers,
   getUserById,
@@ -13,40 +13,14 @@ from "../controllers/userController.js";
 
 const router =
   express.Router();
+// Public Routes
+router.post("/", addUser);          // Register
+router.post("/login", loginUser);   // Login
+router.put("/forgot-password", forgotPassword); // Forgot Password
 
-router.get(
-  "/",
-  getUsers
-);
-
-router.get(
-  "/:id",
-  getUserById
-);
-
-router.put(
-  "/:id",
-  updateUser
-);
-
-router.delete(
-  "/:id",
-  deleteUser
-);
-
-router.post(
-  "/",
-  addUser
-);
-
-router.post(
-  "/login",
-  loginUser
-);
-
-router.put(
-  "/forgot-password",
-  forgotPassword
-);
-
+// Protected Routes
+router.get("/", protect, getUsers);
+router.get("/:id", protect, getUserById);
+router.put("/:id", protect, updateUser);
+router.delete("/:id", protect, deleteUser);
 export default router;
