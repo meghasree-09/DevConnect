b20f50e3bb660147a0bc5f51448a268927b57ce9
meghasree-09 <saimeghanasree09@gmail.com>
2026-07-24 23:14:10 +0530

    add project model

--- Backend/models/Project.js
+++ Backend/models/Project.js
@@ -1,45 +1,64 @@
 import mongoose from "mongoose";
 
-const projectSchema =
-  new mongoose.Schema(
-    {
-      title: {
-        type: String,
-        required: true,
-      },
-
-      description: {
-        type: String,
-        required: true,
-      },
-
-      technologies: [
-        {
-          type: String,
-        },
-      ],
-
-      image: {
-        type: String,
-      },
-
-      githubLink: {
-        type: String,
-      },
-
-      liveLink: {
-        type: String,
-      },
+const projectSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    {
-      timestamps: true,
-    }
-  );
 
-const Project =
-  mongoose.model(
-    "Project",
-    projectSchema
-  );
+    description: {
+      type: String,
+      required: true,
+    },
+
+    technologies: [
+      {
+        type: String,
+      },
+    ],
+
+    image: {
+      type: String,
+    },
+
+    githubLink: {
+      type: String,
+    },
+
+    liveLink: {
+      type: String,
+    },
+
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+   teamMembers: [
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    role: {
+      type: String,
+      default: "Member",
+    },
+
+    joinedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Project = mongoose.model("Project", projectSchema);
 
 export default Project;
