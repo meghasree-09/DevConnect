48ea08723897a2263c4373a9218b1a91be284496
meghasree-09 <saimeghanasree09@gmail.com>
2026-07-24 23:42:56 +0530

    setup express application

--- Backend/server.js
+++ Backend/server.js
@@ -6,17 +6,31 @@ import projectRoutes from "./routes/projectRoutes.js";
 import logger from "./middleware/logger.js";
 import connectDB from "./config/db.js";
 import communityRoutes from "./routes/communityRoutes.js"
+import postRoutes from "./routes/postRoutes.js"
+import chatRoutes from "./routes/chatRoutes.js";
+import eventRoutes from "./routes/eventRoutes.js";
+import teamRoutes from "./routes/teamRoutes.js";
+import taskRoutes from "./routes/taskRoutes.js";
+import notificationRoutes from "./routes/notificationRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
+import resourceRoutes from "./routes/resourceRoutes.js";
 import dotenv from "dotenv";
 import cors from "cors"
+import http from "http";
+import path from "path";
+import { Server } from "socket.io";
 dotenv.config();
 
 
 const app = express();
+const server = http.createServer(app);
 app.use(cors());
 
 connectDB();
 
 app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+app.use(express.urlencoded({ extended: true }));
 app.use(logger);
 
 
@@ -32,7 +46,7 @@ app.use(
 );
 
 app.use("/developers",developerRoutes)
-
+app.use("/resources", resourceRoutes);
 app.use(
   "/teamrequests",
   teamRequestRoutes
@@ -41,7 +55,38 @@ app.use(
 app.use("/communities",communityRoutes);
 
 app.use("/projects", projectRoutes);
+app.use("/uploads", express.static("uploads"));
+app.use("/posts",postRoutes);
+app.use("/chat", chatRoutes);
+app.use("/events", eventRoutes);
+app.use("/team", teamRoutes);
+app.use("/tasks", taskRoutes);
+app.use("/notifications", notificationRoutes);
+app.use("/messages", messageRoutes);
 
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User Connected:", socket.id);
+
+  socket.on("joinProject", (projectId) => {
+    socket.join(projectId);
+    console.log(`User joined project: ${projectId}`);
+  });
+
+  socket.on("sendMessage", (data) => {
+    io.to(data.projectId).emit("receiveMessage", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected:", socket.id);
+  });
+});
 
 // app.use((req, res) => {
 //   res.status(404).json({
@@ -49,8 +94,6 @@ app.use("/projects", projectRoutes);
 //   });
 // });
 
-app.listen(8000, () => {
-  console.log(
-    "Server is  Started at 8000"
-  );
+server.listen(8000, () => {
+  console.log("Server Started at 8000");
 });
