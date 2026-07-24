9b912b4873f311f56fbecfb85c30243ad84b46d6
meghasree-09 <saimeghanasree09@gmail.com>
2026-07-25 01:02:22 +0530

    add api

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,17 @@
+import axios from "axios";
 
-import axios from "axios"
-import React from "react";
 const api = axios.create({
-    baseURL:"http://localhost:8000"
+  baseURL: "http://localhost:8000",
 });
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default api;
