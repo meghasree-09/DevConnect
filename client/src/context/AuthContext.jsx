a0e668767a5394a190d3a557eb45eafe677f8208
meghasree-09 <saimeghanasree09@gmail.com>
2026-07-29 19:57:37 +0530

    implement  auth context

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,43 +1,43 @@
-import { createContext, useContext, useEffect, useState } from "react";
+  import { createContext, useContext, useEffect, useState } from "react";
 
-export const AuthContext = createContext();
+  export const AuthContext = createContext();
 
-export function AuthProvider({ children }) {
-  const [user, setUser] = useState(null);
+  export function AuthProvider({ children }) {
+    const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    const savedUser = localStorage.getItem("user");
+    useEffect(() => {
+      const savedUser = localStorage.getItem("user");
 
-    if (savedUser) {
-      setUser(JSON.parse(savedUser));
-    }
-  }, []);
+      if (savedUser) {
+        setUser(JSON.parse(savedUser));
+      }
+    }, []);
 
-  const login = (userData) => {
-    setUser(userData);
-    localStorage.setItem("user", JSON.stringify(userData));
-  };
+    const login = (userData) => {
+      setUser(userData);
+      localStorage.setItem("user", JSON.stringify(userData));
+    };
 
-  const logout = () => {
-    setUser(null);
-    localStorage.removeItem("user");
-    localStorage.removeItem("token");
-  };
+    const logout = () => {
+      setUser(null);
+      localStorage.removeItem("user");
+      localStorage.removeItem("token");
+    };
 
-  return (
-    <AuthContext.Provider
-      value={{
-        user,
-        setUser,
-        login,
-        logout,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
-}
+    return (
+      <AuthContext.Provider
+        value={{
+          user,
+          setUser,
+          login,
+          logout,
+        }}
+      >
+        {children}
+      </AuthContext.Provider>
+    );
+  }
 
-export function useAuth() {
-  return useContext(AuthContext);
-}
+  export function useAuth() {
+    return useContext(AuthContext);
+  }
